64dcc49d7438f5b59b03386b71f7f2d222a726c8
nuthansai113 <130201140+nuthansai113@users.noreply.github.com>
2023-07-03 22:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function getSize(){
+	 const w = document.documentElement.clientWidth;
+     const h = document.documentElement.clientHeight;
+
+	const elem = document.getElementById('sizeInfo');
+	elem.innerHTML="<h1> Width: "+w+" and Height: "+h+"</h1";
+}
